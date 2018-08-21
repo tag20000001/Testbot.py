b05abb144e99efd7ff40e83d2cb6edf73f44bb7e
tag20000001 <42418221+tag20000001@users.noreply.github.com>
2018-08-22 01:07:18 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -17,30 +17,6 @@ import time
     
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
 
-@bot.command(pass_context=True)
-async def join(ctx):
-	author=ctx.message.author
-	vc=author.voice_channel
-	await bot.join_voice_channel(vc)
-	return
-	
-@bot.command(pass_context=True)
-async def leave(ctx):
-	author=ctx.message.author
-	vc=author.voice_channel
-	await bot.disconnect()
-	return
-	
-@bot.command(pass_context=True)
-async def shutdown(ctx):
-	await bot.close()
-	return
-
-
-    	
-
-
-
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
