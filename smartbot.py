654bd9615ca10337f17cc28ad5f03bf9013195eb
tag20000001 <42418221+tag20000001@users.noreply.github.com>
2018-09-25 02:10:13 +0530

    Update smartbot.py

--- smartbot.py
+++ smartbot.py
@@ -202,5 +202,19 @@ async def bans(ctx):
     if not ban_list:
     	
         await bot.say('Ban list is empty.')
-        return     
+        return 
+@bot.command(pass_context=True)
+async def join(ctx):
+    author=ctx.message.author
+    vc=author.voice_channel
+    await bot.join_voice_channel(vc)
+    return
+
+@bot.command(pass_context=True)
+async def leave(ctx):
+    server=ctx.message.server
+    voice_client=bot.voice_client_in(server)
+    await voice_client.disconnect()
+    return
+
 bot.run(os.getenv('Token'))
