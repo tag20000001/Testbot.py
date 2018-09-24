2249cc9111183ba8ba27d6bd9ea68c7f4b0d1f0e
tag20000001 <42418221+tag20000001@users.noreply.github.com>
2018-09-24 23:30:54 +0530

    Update smartbot.py

--- smartbot.py
+++ smartbot.py
@@ -6,7 +6,7 @@ import platform
 import time
 import os
 import logging
-import opus
+import Opus
 print('=========================================')
 print('#Information: ')
 logging.basicConfig(level=logging.INFO)
