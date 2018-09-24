9716c1b7c28c475daa90773982db117e70640837
tag20000001 <42418221+tag20000001@users.noreply.github.com>
2018-09-25 01:19:41 +0530

    Update smartbot.py

--- smartbot.py
+++ smartbot.py
@@ -6,7 +6,7 @@ import platform
 import time
 import os
 import logging
-import opuslib
+import youtube_dl
 print('=========================================')
 print('#Information: ')
 logging.basicConfig(level=logging.INFO)
