49a9462d16f7ec85ec068a4b1149cb2909e37549
tag20000001 <42418221+tag20000001@users.noreply.github.com>
2018-12-01 21:19:14 +0530

    Update smartbot.py

--- smartbot.py
+++ smartbot.py
@@ -237,6 +237,6 @@ async def go(ctx):
     author=ctx.message.author
     vc=author.voice_channel
     voice_client=bot.voice_client_in(server)
-    await voice_client.bot.disconnect()
+    await bot.disconnect(voice_client)
 
 bot.run(os.getenv('Token'))
