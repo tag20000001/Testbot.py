9eb1b68068c1ef07cf17ec7a689b5b28fdb10695
tag20000001 <42418221+tag20000001@users.noreply.github.com>
2018-12-16 17:07:50 +0530

    Update advchat.py

--- advchat.py
+++ advchat.py
@@ -18,31 +18,38 @@ async def on_ready():
 	print('=========================================')
 	
 	print('advchat.py is on')
+#new improved chat
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
-    user='{0.author.name}'.format(message)
-    hlo ='Hello!  {0.author}'.format(message)
-    greetmsg=["Hi ","Hello ","Hey ","Yo "]
-    usergreet=["Hi" or "Hello" or "Hey" or "Yo"]
-    ht=["Fine","Well","Felling little tired","Good"]
+	user='{0.author.name}'.format(message)
+	greetmsg=["Hi ","Hello ","Hey ","Yo "]
+	msg3=['Bye ','Bye for now ']
+	msghru=['Fine and what about you ','Felling well :D and what about you ','Good and what about you ']
+	emj=[':ok_hand:',':thumbsup:']
+	if message.author is bot.user:
+		return
+	elif message.content.startswith("."):
+		return
+	elif 'hi' in message.content or 'hello' in message.content or 'yo <@450220085676605441>' in message.content or 'hey' in message.content or 'Hi' in message.content or 'Hello' in message.content or 'Yo <@450220085676605441>'  in message.content or '<@450220085676605441>Yo' in message.content or '<@450220085676605441>yo'  in message.content  or '<@450220085676605441> Yo' in message.content or '<@450220085676605441> yo'  in message.content  or 'Hey' in message.content:
+		msg = await  bot.send_message(message.channel,embed=discord.Embed(title=random.choice(greetmsg)+user,color=0x00ff00))
+		return
+		
+	elif 'bye' in message.content or 'Bye' in message.content:
+		msg = await  bot.send_message(message.channel,embed=discord.Embed(title=random.choice(msg3)+user,color=0x00ff00))
+		return		
+	elif 'how are you' in message.content or 'How are you' in message.content or 'how r u' in message.content or 'How r u' in message.content:
+		msg = await  bot.send_message(message.channel,embed=discord.Embed(title=random.choice(msghru)+user+'? ',color=0x00ff00))
+		return
 	
+	elif 'fine' in message.content or 'Fine' in message.content or 'well' in message.content or 'Well' in message.content or 'good' in message.content or 'Good' in message.content:
+		await bot.add_reaction(message,'👍')
 	
-    if message.content.startswith("."):
-    	return
-     
+	elif 'Not fine' in message.content or 'not fine' in message.content or 'Not well' in message.content or 'not well' in message.content or 'Not good' in message.content or 'not good' in message.content or 'tired' in message.content or 'Tired' in message.content or 'sick' in message.content or 'Sick' in message.content:
+		await bot.add_reaction(message,'😔')
 
-    if message.content.startswith("Hi")or message.content.startswith("Hello")or message.content.startswith("Hey")or message.content.startswith("Yo")or message.content.endswith("Hi")or message.content.endswith("Hello")or message.content.endswith("Hey")or message.content.endswith("Yo")or message.content.startswith("hi")or message.content.startswith("hello")or message.content.startswith("hey")or message.content.startswith("yo")or message.content.endswith("hi")or message.content.endswith("hello")or message.content.endswith("hey")or message.content.endswith("yo"):
-    	await bot.send_message(message.channel,embed=discord.Embed(title=random.choice(greetmsg)+user,color=0x00ff00))
-    if message.content.startswith('Bye')or message.content.startswith("bye")or message.content.endswith("Bye")or message.content.endswith("bye"):
-    	await bot.send_message(message.channel,embed=discord.Embed(title='Bye '+user,color=0x00ff00))    	    	
-    	return
-    if message.content.startswith("how are you?")or message.content.startswith("How r u?")or message.content.startswith("how are you")or message.content.startswith("How are you?")or message.content.startswith("How are you")or message.content.startswith("how r u?")or message.content.endswith("how are you")or message.content.endswith("How are you?")or message.content.endswith("How are you?")or message.content.endswith("How r u?")or message.content.endswith("How are you")or message.content.endswith("how r u?"):
-    		await bot.send_message(message.channel,embed=discord.Embed(title=random.choice(ht)+user,color=0x00ff00))
     	
     		
-    		return   
+    
       	   	
 bot.run(os.getenv('Token'))
 		
